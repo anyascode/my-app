b23fde6fb10602cecbdcf4e9faa13b1a771bad00
Anya Pham <kieuanh997@gmail.com>
2025-02-26 19:15:06 +0300

    создан проект, перенесена верстка, разделила верстку на компоненты, реализовала передачу тасков через пропсы

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types"; // Импорт PropTypes
+import Task from "../Task/Task";
+
+function TaskList({ tasks }) {
+  return (
+    <ul className="todo-list">
+      {tasks.map((item) => (
+        <li className={item.className || ""} key={item.id}>
+          <Task />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+TaskList.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      className: PropTypes.string,
+    })
+  ).isRequired,
+};
+
+export default TaskList;
---
+++ src/components/ToDoApp/ToDoApp.jsx
@@ -0,0 +1,26 @@
+import TaskList from "../TaskList/TaskList";
+import NewTaskForm from "../NewTaskForm/NewTaskForm";
+import Footer from "../Footer/Footer";
+
+function ToDoApp() {
+  const tasksData = [
+    { id: 1, className: "completed" },
+    { id: 2, className: "editing" },
+    { id: 3, className: null },
+  ];
+
+  return (
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+        <NewTaskForm />
+      </header>
+      <section className="main">
+        <TaskList tasks={tasksData} />
+        <Footer />
+      </section>
+    </section>
+  );
+}
+
+export default ToDoApp;
